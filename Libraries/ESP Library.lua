d3a45d7b047eda44bcbc449d3d67d83d9204953b
Hexnet <41995259+Hexnet111@users.noreply.github.com>
2022-07-21 19:03:34 +0300

    Added How to use

--- Libraries/ESP Library.lua
+++ Libraries/ESP Library.lua
@@ -1,8 +1,37 @@
+--[[
+	How to use:
+		
+		ObjectData Example:
+			{
+				CustomColor : Color3 (Color for the ESP)
+				CustomName : string (Name displayed on the item)
+				Name : string (The Name of the object that the script should look for) (Required)
+				ClassName : string (The ClassName of the object the script should look for) (Required)
+			}
+
+		CustomFuncs:
+			CustomRemoval:
+				A function that is given an object and has to return an event.
+				Please use Module:RemoveESP(Object) to remove the esp.
+				Also make sure that the event will be disconnected after use to avoid memory leaks.
+
+			CustomCheck:
+				A function that will be used to perform a custom check on an object.
+				Useful when just the Name and the Classname are not enough.
+				Needs to return a true when the correct object is found.
+
+
+		DecendantCheck or ChildCheck:
+			Module:DescendantCheck(Where to search, ObjectData, CustomFuncs)
+
+			(Note: it's not required to use these functions, they're just there as a quick way to test things.)
+]]
+
+
 --//Services
 local Players = game:GetService("Players")
 local CoreGui = game:GetService("CoreGui")
 local UserInputService = game:GetService("UserInputService")
-local Debris = game:GetService("Debris")
 
 
 --//Variables
@@ -60,13 +89,21 @@ end
 
 --//Functions
 function Module:StopScript()
+	for Object, _ in pairs(Pickables) do
+		Module:RemoveESP(Object)
+	end
+
 	RemoveAllConnections()
+
+	print("ESP Library Stopped!")
 end
 
-function Module:DescendantCheck(MainParent, ObjectData, CustomCheck)
+function Module:DescendantCheck(MainParent, ObjectData, CustomFuncs)
+ 	CustomFuncs = CustomFuncs or {}
+
 	local function ObjectAdded(Object)
-		if CustomCheck and typeof(CustomCheck) == "function" and CustomCheck(Object, ObjectData) or Object.Name == ObjectData.Name and Object.ClassName == ObjectData.ClassName then
-			Module:ESPObject(Object, ObjectData.CustomName or Object.Name, ObjectData.CustomColor or GetObjectColor(Object))
+		if CustomFuncs.CustomCheck and typeof(CustomFuncs.CustomCheck) == "function" and CustomFuncs.CustomCheck(Object, ObjectData) or Object.Name == ObjectData.Name and Object.ClassName == ObjectData.ClassName then
+			Module:ESPObject(Object, ObjectData.CustomName or Object.Name, ObjectData.CustomColor or GetObjectColor(Object), CustomFuncs.CustomRemoval)
 		end
 	end
 
@@ -77,10 +114,12 @@ function Module:DescendantCheck(MainParent, ObjectData, CustomCheck)
 	AddConnection(MainParent.DescendantAdded:Connect(ObjectAdded))
 end
 
-function Module:ChildCheck(MainParent, ObjectData, CustomCheck)
+function Module:ChildCheck(MainParent, ObjectData, CustomFuncs)
+	CustomFuncs = CustomFuncs or {}
+
 	local function ObjectAdded(Object)
-		if CustomCheck and typeof(CustomCheck) == "function" and CustomCheck(Object, ObjectData) or Object.Name == ObjectData.Name and Object.ClassName == ObjectData.ClassName then
-			Module:ESPObject(Object, ObjectData.CustomName or Object.Name, ObjectData.CustomColor or GetObjectColor(Object))
+		if CustomFuncs.CustomCheck and typeof(CustomFuncs.CustomCheck) == "function" and CustomFuncs.CustomCheck(Object, ObjectData) or Object.Name == ObjectData.Name and Object.ClassName == ObjectData.ClassName then
+			Module:ESPObject(Object, ObjectData.CustomName or Object.Name, ObjectData.CustomColor or GetObjectColor(Object), CustomFuncs.CustomRemoval)
 		end
 	end
 
@@ -117,21 +156,38 @@ function Module:ActivateAntiAFK()
     end
 end
 
-function Module.ESPObject(Object,Name,Color)
+function Module:ESPObject(Object, Name, Color, CustomRemoval)
     local Clone = BillboardGui:Clone()
     Clone.Parent = CoreGui
     Clone.Adornee = Object
     Clone.TextLabel.Text = Name
-    Clone.Size = UDim2.new(20,#Name*2,5,#Name)
+    Clone.Size = UDim2.new(8, #Name * 4, 3, #Name * 2)
 
     if Color then
         Clone.TextLabel.TextColor3 = Color
     end
 
     Pickables[Object] = Clone
+
+    do
+    	if CustomRemoval then
+    		AddConnection(CustomRemoval(Object))
+    	else
+    		local Connection 
+
+	    	Connection = AddConnection(Object:GetPropertyChangedSignal("Parent"):Connect(function()
+	    		if Object.Parent == nil then
+					print("Object Removed")
+
+		    		Module:RemoveESP(Object)
+		    		Connection:Disconnect()
+	    		end
+	    	end))
+    	end
+    end
 end
 
-function Module.RemoveObject(Object)
+function Module:RemoveESP(Object)
     if not Pickables[Object] then return end
 
     Pickables[Object]:Destroy()
@@ -140,7 +196,7 @@ end
 
 
 --//Events
-AddConnection(UserInputService.InputBegan:Connect(function()
+AddConnection(UserInputService.InputBegan:Connect(function(Input)
 	if Input.KeyCode ~= Settings.StopKEY then return end
 
 	Module:StopScript()
@@ -148,6 +204,22 @@ end))
 
 
 --//Script
+BillboardGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+BillboardGui.Active = true
+BillboardGui.AlwaysOnTop = true
+BillboardGui.LightInfluence = 1.000
+BillboardGui.Size = UDim2.new(20, 0, 5, 0)
+TextLabel.Parent = BillboardGui
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Size = UDim2.new(1, 0, 1, 0)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.TextColor3 = Color3.fromRGB(255, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextStrokeTransparency = 0.500
+TextLabel.TextWrapped = true
+
 return function(NewSettings)
 	if NewSettings then
 		for Setting, Value in pairs(Settings) do
